68e5a254322ab391611370c7e9ad9b743d60f19c
robario <webmaster@robario.com>
2013-08-28 11:40:15 +0900

    Add an inverse feature

--- github-diff-ws.user.js
+++ github-diff-ws.user.js
@@ -6,11 +6,24 @@
     if (!toc) {
         return;
     }
+
     var a = document.createElement('a');
-    a.innerText = 'Ignore WS';
     a.setAttribute('class', 'minibutton');
-    a.onclick = function() {
-        location.search += location.search ? '&w=' : '?w=';
-    };
+    if (/[&?]w=/.test(location.search)) {
+        a.innerText = 'Cognize WS';
+        a.onclick = function() {
+            var search = location.search.replace(/[&?]w=/g, '');
+            if (search) {
+                location.search = search;
+            } else {
+                location.href = location.pathname + location.hash;
+            }
+        };
+    } else {
+        a.innerText = 'Ignore WS';
+        a.onclick = function() {
+            location.search += location.search ? '&w=' : '?w=';
+        };
+    }
     toc.getElementsByClassName('explain')[0].insertBefore(a, toc.getElementsByClassName('show-diff-stats')[0]);
 })();
