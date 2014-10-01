c038afe03a4cf57b6bd53135494536e5852401ff
robario <webmaster@robario.com>
2014-10-01 14:37:55 +0900

    Fix to follow GitHub changes.

--- github-diff-ws.user.js
+++ github-diff-ws.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name github-diff-ws.user.js
 // @namespace http://www.robario.com/
-// @version 0.1.0
+// @version 0.2.0
+// @author robario <webmaster@robario.com>
+// @description Add a button to be able to toggle whitespace ignoring.
 // @updateURL https://raw.githubusercontent.com/robario/github-diff-ws/master/github-diff-ws.user.js
 // @match https://github.com/*/*/commit/*
 // @match https://github.com/*/*/compare/*
@@ -15,18 +17,18 @@
 
     var updateUI = function(toc) {
         var a = document.createElement('a');
-        a.setAttribute('class', 'minibutton');
+        a.innerText = 'Ignore WS';
+        a.className = 'minibutton';
         var search = location.search.replace(/[&?]w=1(&|$)/g, '$1').replace(/^&/, '?');
         if (search == location.search) {
-            a.innerText = 'Ignore WS';
             search += (search ? '&' : '?') + 'w=1';
         } else {
-            a.innerText = 'Cognize WS';
+            a.className += ' selected';
         }
         a.onclick = function() {
             location.href = location.protocol + '//' + location.host + location.pathname + search + location.hash;
-        }
-        toc.getElementsByClassName('explain')[0].insertBefore(a, toc.getElementsByClassName('show-diff-stats')[0]);
+        };
+        toc.getElementsByClassName('button-group')[0].appendChild(a);
     };
 
     document.getElementById('js-repo-pjax-container').addEventListener('DOMNodeInserted', function(event) {
