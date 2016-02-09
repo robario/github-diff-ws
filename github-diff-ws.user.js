1747cb6560106d83382b06b45b0bddc54627e9ea
robario <webmaster@robario.com>
2016-02-09 15:17:29 +0900

    Fix to follow GitHub changes

--- github-diff-ws.user.js
+++ github-diff-ws.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name github-diff-ws.user.js
 // @namespace http://www.robario.com/
-// @version 0.2.1
+// @version 0.2.2
 // @author robario <webmaster@robario.com>
 // @description Add a button to be able to toggle whitespace ignoring.
 // @updateURL https://raw.githubusercontent.com/robario/github-diff-ws/master/github-diff-ws.user.js
@@ -19,7 +19,7 @@
     var updateUI = function(toc) {
         var a = document.createElement('a');
         a.innerText = 'Ignore WS';
-        a.className = 'minibutton';
+        a.className = 'btn btn-sm';
         var search = location.search.replace(/[&?]w=1(&|$)/g, '$1').replace(/^&/, '?');
         if (search == location.search) {
             search += (search ? '&' : '?') + 'w=1';
@@ -29,7 +29,7 @@
         a.onclick = function() {
             location.href = location.protocol + '//' + location.host + location.pathname + search + location.hash;
         };
-        toc.getElementsByClassName('button-group')[0].appendChild(a);
+        toc.getElementsByClassName('btn-group')[0].appendChild(a);
     };
 
     document.getElementById('js-repo-pjax-container').addEventListener('DOMNodeInserted', function(event) {
