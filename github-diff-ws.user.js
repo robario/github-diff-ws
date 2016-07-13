fa1229c88a1a351c735bd98ff95a7eabeaffcda2
robario <webmaster@robario.com>
2016-07-13 09:43:07 +0900

    :+1: Fix can run to start from anywhere

--- github-diff-ws.user.js
+++ github-diff-ws.user.js
@@ -1,23 +1,27 @@
 // ==UserScript==
 // @name github-diff-ws.user.js
 // @namespace http://www.robario.com/
-// @version 0.2.2
+// @version 0.2.3
 // @author robario <webmaster@robario.com>
 // @description Add a button to be able to toggle whitespace ignoring.
 // @updateURL https://raw.githubusercontent.com/robario/github-diff-ws/master/github-diff-ws.user.js
-// @match https://github.com/*/*/commit/*
-// @match https://github.com/*/*/compare/*
-// @match https://github.com/*/*/pull/*/files*
+// @match https://github.com/*
 // @grant none
 // ==/UserScript==
 (function() {
-    var toc = document.getElementById('toc');
-    if (!toc) {
-        return;
-    }
-
-    var updateUI = function(toc) {
-        var a = document.createElement('a');
+    var a;
+    var updateUI = function() {
+        if (a) {
+            return;
+        }
+        var toc = document.getElementById('toc');
+        if (!toc) {
+            return;
+        }
+        if (!location.pathname.match(new RegExp('^/.+?/.+?/(?:commit|compare)/'))) {
+            return;
+        }
+        a = document.createElement('a');
         a.innerText = 'Ignore WS';
         a.className = 'btn btn-sm';
         var search = location.search.replace(/[&?]w=1(&|$)/g, '$1').replace(/^&/, '?');
@@ -32,10 +36,6 @@
         toc.getElementsByClassName('btn-group')[0].appendChild(a);
     };
 
-    document.getElementById('js-repo-pjax-container').addEventListener('DOMNodeInserted', function(event) {
-        if (event.target.id == 'toc') {
-            updateUI(event.target);
-        }
-    }, false);
-    updateUI(toc);
+    document.addEventListener('pjax:end', updateUI, false);
+    updateUI();
 })();
